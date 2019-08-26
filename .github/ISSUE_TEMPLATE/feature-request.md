754eae3cc1660e19438d7cf403984bea2a005063
Jay Rathi <jrathi@microsoft.com>
2019-08-26 10:54:10 -0700

    [COMMIT COMMON]

---
+++ .github/ISSUE_TEMPLATE/feature-request.md
@@ -0,0 +1,13 @@
+---
+name: Feature request
+about: To request a new feature in office.js or any other aspect of the Office developer
+  platform, post your request to User Voice at https://officespdev.uservoice.com/
+title: ''
+labels: ''
+assignees: ''
+
+---
+
+>>> PLEASE USE USER VOICE FOR FEATURE REQUESTS <<<
+
+Post feature requests at https://officespdev.uservoice.com/
---
+++ deployment.script/src/debug.ts
@@ -0,0 +1,29 @@
+import { EnvironmentVariables } from "./EnvironmentVariables";
+import * as standardFile from "./standardFile";
+import { executeCommand } from "./executeCommand";
+
+// const out = executeCommand(`npm view "@microsoft/office-js" versions --json`, `R:\\office-js-api\\tools`, true);
+// console.log(out);
+
+export function debug(env: EnvironmentVariables) {
+  // Print environment variables
+  const fieldsToPrint: (keyof EnvironmentVariables)[] = [
+    "TRAVIS",
+    "TRAVIS_BRANCH",
+    "TRAVIS_PULL_REQUEST",
+  ];
+
+  const fields = fieldsToPrint.map((item) => `"${item}": "${env[item]}"`).join(",\n");
+  console.log(fields);
+
+  console.log(`subdirectories: [${env.TRAVIS_BUILD_DIR}]`);
+  console.log(standardFile.getSubDirectories(env.TRAVIS_BUILD_DIR));
+}
+
+export function banner(title: string): string {
+  const border = "-".repeat(title.length);
+  const format = `${border}
+${title}
+${border}`;
+  return format;
+}
