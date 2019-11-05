1d2f54517aaf73b380517f3bf88f944728caf8f7
Jay Rathi <jrathi@microsoft.com>
2019-11-04 17:50:55 -0800

    [COMMIT COMMON]

---
+++ deployment.script/src/index.ts
@@ -0,0 +1,82 @@
+import * as environment from "./EnvironmentVariables";
+import * as standardFile from "./standardFile";
+
+import { EnvironmentVariables } from "./EnvironmentVariables";
+import { debug, banner } from "./debug";
+import { deploymentPrerequisitesPassed } from "./deploymentPrerequisitesPassed";
+import { getReleaseTypeFromBranchName, ReleaseType } from "./ReleaseType";
+import { getNpmPackageTag } from "./getNpmPackageTag";
+import { deployNpmPackage } from "./deployNpmPackage";
+import { isUndefined } from "util";
+
+/*
+General Overview:
+
+The script runs on any commit to a branch.
+
+Based on the branch name a different deployment is triggered.
+
+Three types of packages:
+
+    version: corresponds to the npm package version (needs to be set in the package.json)
+    tag: corresponds to the npm package tag
+
+release:
+    version: x.y.z
+    tag: release
+
+beta:
+    version: x.y.(z+1)-beta.q
+    tag: beta
+
+custom:
+    version: x.y.(z+1)-custom.p
+    tag: custom
+*/
+console.log("Deployment Script: Start");
+const env: environment.EnvironmentVariables = environment.getEnvironmentVariables();
+
+// Printing for debug purposes
+// debug(env);
+
+if (!deploymentPrerequisitesPassed(env)) {
+  process.exit(0);
+}
+
+// Base actions on the branch name
+const release_type: ReleaseType = getReleaseTypeFromBranchName(env.TRAVIS_BRANCH);
+const tag = getNpmPackageTag(release_type);
+
+const packageDirectory = env.TRAVIS_BUILD_DIR;
+const packageName = "@microsoft/office-js"; // could pull from the package.json
+const packageTag = tag;
+const npmAuthToken = env.NPM_TOKEN;
+
+// TODO: make it throw on failure instead
+const deployedPackageVersion: string | undefined = deployNpmPackage(
+  packageDirectory,
+  packageName,
+  packageTag,
+  npmAuthToken,
+);
+console.log("Deployment Script: Complete");
+
+const deploymentSucceeded = !isUndefined(deployedPackageVersion);
+// report in an extra obvious way
+console.log(banner(`DEPLOYMENT [${deploymentSucceeded ? "SUCCEEDED" : "FAILED"}]`));
+if (deploymentSucceeded) {
+  console.log(`
+
+Unpkg CDN URLs:
+
+https://unpkg.com/@microsoft/office-js@${deployedPackageVersion}/dist/office.js
+https://unpkg.com/@microsoft/office-js@${deployedPackageVersion}/dist/office.d.ts
+
+
+
+Unpkg CDN Debug URLs:
+
+https://unpkg.com/@microsoft/office-js@${deployedPackageVersion}/dist/office.debug.js
+https://unpkg.com/@microsoft/office-js@${deployedPackageVersion}/dist/office-with-first-party.d.ts
+`);
+}
---
+++ deployment.script/src/standardFile.ts
@@ -0,0 +1,68 @@
+import * as fs from "fs";
+import * as path from "path";
+
+export function standardNewlines(s: string): string {
+  return s.replace(/\r/gm, "");
+}
+
+/**
+ * Read utf-8 file and transform to standard new lines.
+ * @param path file path
+ */
+export function readFile(path: string): string {
+  return standardNewlines(fs.readFileSync(path, "utf-8"));
+}
+
+/**
+ * write data to path with standard newlined.
+ * @param path
+ * @param data
+ */
+export function writeFile(path: string, data: string) {
+  const cleanData = standardNewlines(data);
+  fs.writeFileSync(path, cleanData);
+}
+
+/**
+ * Read a file that contains json and turn it into an object
+ *
+ * Note: no validation is done on the data.
+ * TODO: add validation of json to ensure it conforms to schema to type.
+ *
+ * @param path path to the json file
+ */
+export function readFileJson<T>(path: string): T {
+  const data: string = readFile(path);
+  const object: T = JSON.parse(data);
+  return object;
+}
+
+export function isDirectory(directory: string): boolean {
+  return fs.lstatSync(directory).isDirectory();
+}
+
+function isFile(file: string): boolean {
+  return fs.lstatSync(file).isFile();
+}
+
+/**
+ * Transform a data object to a string and write it to the specified path.
+ * @param path
+ * @param data
+ */
+export function writeFileJson(path: string, data: {}): void {
+  const json: string = JSON.stringify(data, undefined, 2);
+  writeFile(path, json);
+}
+
+export function getSubDirectories(directory: string): string[] {
+  const all = fs.readdirSync(directory);
+  const directories = all.filter((sub) => isDirectory(path.join(directory, sub)));
+  return directories.sort();
+}
+
+export function getFilesInDirectory(directory: string): string[] {
+  const all = fs.readdirSync(directory);
+  const files = all.filter((file) => isFile(path.join(directory, file)));
+  return files.sort();
+}
